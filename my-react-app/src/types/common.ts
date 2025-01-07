2b72b4188ce0f070ef77a660a2905b23692d3ee1
Machiko-Hashiguchi <machinoko.hassy@gmail.com>
2025-01-08 00:15:59 +0900

    my-react-appフォルダ削除

--- my-react-app/src/types/common.ts
+++
@@ -1,4 +0,0 @@
-// types/common.ts
-export type Color = "blue" | "red" | "green" | "amber" | "blue-gray";
-export type Variant = "gradient" | "filled" | "outlined" | "text";
-export type Size = "sm" | "md" | "lg";
